536466c77b890b497164cbb89941d2d8135b44c4
josebormey <josebormey@gmail.com>
2022-01-20 14:48:04 -0600

    alumno guardado

---
+++ app/Models/Alumno.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alumno extends Model
+{
+    private $table = "alumno";
+    
+    use HasFactory;
+
+   
+}
